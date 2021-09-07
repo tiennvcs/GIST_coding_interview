28511e4266147ad43dc7e45542b84fbb1266f2d4
tiennvuit <tiennvuit@gmail.com>
2021-09-07 08:34:42 +0700

    add script runing

---
+++ runs/script_run/run_rnn_64.sh
@@ -0,0 +1,77 @@
+python train.py --data_dir ./data --model RNN \
+--pretrain_embedding data/all.review.vec.txt \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-3 --optimizer adam \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_Adam_1e3_Adam_w/
+
+python train.py --data_dir ./data --model RNN \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-3 --optimizer adam \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_Adam_1e3_Adam_wo/
+
+python train.py --data_dir ./data --model RNN \
+--pretrain_embedding data/all.review.vec.txt \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-3 --optimizer sgd \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_SGD_1e3_w/
+
+python train.py --data_dir ./data --model RNN \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-3 --optimizer sgd \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_SGD_1e3_wo/
+
+python train.py --data_dir ./data --model RNN \
+--pretrain_embedding data/all.review.vec.txt \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-3 --optimizer rmsprop \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_RMSProp_1e3_w/
+
+python train.py --data_dir ./data --model RNN \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-3 --optimizer rmsprop \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_RMSProp_1e3_wo/
+
+python train.py --data_dir ./data --model RNN \
+--pretrain_embedding data/all.review.vec.txt \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-2 --optimizer adam \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_Adam_1e2_Adam_w/
+
+python train.py --data_dir ./data \--model RNN \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-2 --optimizer adam \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_Adam_1e2_Adam_wo/
+
+python train.py --data_dir ./data --model RNN \
+--pretrain_embedding data/all.review.vec.txt \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-2 --optimizer sgd \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_SGD_1e2_w/
+
+python train.py --data_dir ./data --model RNN \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-2 --optimizer sgd \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_SGD_1e2_wo/
+
+python train.py --data_dir ./data --model RNN \
+--pretrain_embedding data/all.review.vec.txt \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-2 --optimizer rmsprop \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_RMSProp_1e2_w/
+
+python train.py --data_dir ./data --model RNN \
+--num_words 10000 --feature_dim 100 \
+--learning_rate 1e-2 --optimizer rmsprop \
+--epochs 100 --batch_size 64 \
+--work_dir ./runs/train/RNN_64_RMSProp_1e2_wo/
